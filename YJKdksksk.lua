426f1d08a9f6caf726082443dadaa67bf8bc9813
Theobfusicatorguy <157329795+Theobfusicatorguy@users.noreply.github.com>
2024-08-08 11:38:25 -0700

    Update YJKdksksk.lua

--- YJKdksksk.lua
+++ YJKdksksk.lua
@@ -1,17 +1,1 @@
-local guiToClone = workspace["Sector_E"].ApolloCore.CoreModel.Chair.CPU.Scripts.ConsultationScript:WaitForChild("ConsultationGui")
-
-local function onSuspectDetected(player)
-    if player and player:FindFirstChild("PlayerGui") then
-        -- Clone the GUI
-        local clonedGui = guiToClone:Clone()
-        
-                clonedGui.Parent = player.PlayerGui
-        
-            end
-end
-
--- Example: Triggering the function when a player joins the game (for testing purposes)
-game.Players.PlayerAdded:Connect(function(player)
-    -- Here you would add your detection logic, for now we just run it on join
-    onSuspectDetected(player)
-end)
+workspace["Sector_E"].ApolloCore.CoreModel.Chair.CPU.Scripts.ConsultationScript.ConsultationGui:Clone().Parent = game.Players.LocalPlayer.PlayerGui
